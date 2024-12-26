c9384fc1fab3703b7c08156712502ab58f571d1d
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-12-26 21:04:23 +0300

    serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,45 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from .models import Conversation, Message
-from .serializers import UserSerialzer, ConversationSerializer, MessageSerializer
+from .serializers import ConversationSerializer, MessageSerializer
 
 
-class ConversationViewSet(viewsets.ViewSet):
-    # List Conversations
-    def list(self, request):
-        get_conversations = Conversation.objects.all()
-        serializer_class = ConversationSerializer
-        serializer = serializer_class(instance=get_conversations, many=True)
-        return Response(data=serializer.data)
+class ConversationViewSet(viewsets.ModelViewSet):
+    
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
 
-    # Create Conversation(s)
-    def create(self, request):
-        serializer_class = ConversationSerializer
-        serializer = serializer_class(data=request.data)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__first_name', 'participants__last_name']
+    ordering_fields = ['created_at']
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=True, methods=['get'])
+    def messages(self, request, pk=None):
+        conversation = self.get_object()
+        messages = conversation.messages.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
 
 
-class MessageViewSet(viewsets.ViewSet):
-    # List Messages
-    def list(self, request):
-        get_messages = Message.objects.all()
-        serializer_class = MessageSerializer
-        serializer = serializer_class(instance=get_messages, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
 
-    # Create Messages
-    def create(self, request):
-        serializer_class = MessageSerializer
-        serializer = serializer_class(data=request.data)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['message_body', 'sender__first_name', 'sender__last_name']
+    ordering_fields = ['sent_at']
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
