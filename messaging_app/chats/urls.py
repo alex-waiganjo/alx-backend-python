0acbcadce4235afc74c958ec12af20ddc2c65214
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-12-26 21:06:18 +0300

    routes setup

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router =DefaultRouter()
 router = DefaultRouter()
 router.register(r'conversation', ConversationViewSet, basename='conversation')
 router.register(r'message', MessageViewSet, basename='message')
