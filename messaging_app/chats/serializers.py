597796ad9b08dc160c264b7eb61ff4827912c52f
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-12-26 20:40:36 +0300

    more serializer changes

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,21 +3,25 @@ from .models import User, Message, Conversation
 
 
 class UserSerialzer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    email = serializers.EmailField(max_length=50)
+    password_hash = serializers.CharField(max_length=100)
+    phone_number = serializers.CharField(max_length=20)
+    role = serializers.CharField(max_length=10)
+    created_at = serializers.DateTimeField()   
 
     class Meta:
         model = User
-        fileds = ['user_id','first_name','last_name','email','password_hash','phone_number','role','created_at','full_name']
+        fileds = ['user_id','first_name','last_name','email','password_hash','phone_number','role','created_at']
 
-        def full_name(self,obj):
-            return f'{obj.first_name} {obj.last_name}'
         
         def validation_email(self, value):
             if '@yahoo.com' in value:
                 raise serializers.ValidationError("Domain is unacceptable")
             return value
         
-        
+
        
 
 
